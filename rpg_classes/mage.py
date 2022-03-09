b57fee2f01b3e2bfda58dabed1ce357a0969e17c
RobsonMT <rbsndev3@gmail.com>
2022-03-08 22:31:22 -0300

    feat: rpg with inheritance

--- rpg_classes/mage.py
+++ rpg_classes/mage.py
@@ -1,1 +1,29 @@
-# Desenvolva sua classe Mage abaixo
+from rpg_classes.villager import Villager
+
+
+class Mage(Villager):
+    def __init__(self, name):
+        super().__init__(name)
+        self.attack = 10
+        self.mana = 100
+
+    def fire_ball(self, target: "Villager"):
+        manacost = 25
+        damage = 30
+
+        if not target.is_alive:
+            return f"{target.name} ja esta morto!!"
+
+        if self.mana < manacost:
+            return "Mana insuficiente"
+
+        dmg = damage - target.defense
+
+        if dmg > 0:
+            self.mana -= manacost
+            target.check_health(dmg)
+
+        if target.health > 0:
+            return f"{target.name} ficou com {target.health} de vida"
+
+        return f"{target.name} morreu !!"
--- rpg_classes/villager.py
+++ rpg_classes/villager.py
@@ -1,1 +1,26 @@
-# Desenvolva sua classe Villager abaixo
+class Villager:
+    def __init__(self, name):
+        self.name = name
+        self.health = 50
+        self.defense = 0
+        self.attack = 0
+        self.is_alive = True
+
+    def check_health(self, target):
+        if self.health - target <= 0:
+            self.health = 0
+            self.is_alive = False
+        else:
+            self.health -= target
+
+    def normal_attack(self, target: "Villager"):
+        if target.health == 0:
+            return f"{target.name} ja esta morto!!"
+
+        if self.attack - target.defense > 0:
+            target.health = self.attack - target.defense
+
+        if target.health > 0:
+            return f"{target.name} ficou com {target.health} de vida"
+
+        return f"{target.name} morreu !!"
